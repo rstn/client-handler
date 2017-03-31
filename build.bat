7f224a17b15ac99bcbdf03818130d98b1e2873d7
Petr <stone.ptr@gmail.com>
2017-03-31 22:49:14 +0400

    тестовые данные

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
+rd /s /q  src\test\resources\inbox
+rd /s /q  src\test\resources\outbox
+rd /s /q  src\test\resources\processed
+xcopy src\assembly\inbox_test src\test\resources\inbox\
 mvn clean install
