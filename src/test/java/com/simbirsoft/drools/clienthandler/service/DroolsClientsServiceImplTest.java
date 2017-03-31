91d161c6ec2389b982d2d93d125f00f735852086
Dmitry Lubimov <dmitry.lubimov@simbirsoft.com>
2017-03-31 20:45:26 +0400

    Test added

--- src/test/java/com/simbirsoft/drools/clienthandler/service/DroolsClientsServiceImplTest.java
+++ src/test/java/com/simbirsoft/drools/clienthandler/service/DroolsClientsServiceImplTest.java
@@ -4,6 +4,7 @@ import com.simbirsoft.drools.clienthandler.model.Client;
 import com.simbirsoft.drools.clienthandler.model.ClientResult;
 import com.simbirsoft.drools.clienthandler.model.DroolsClient;
 import com.simbirsoft.drools.clienthandler.model.Subscriber;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,19 +17,20 @@ import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:testApplicationContext.xml"})
 public class DroolsClientsServiceImplTest {
 
-    @Autowired
-    @Qualifier("DroolsClientsServiceImpl")
-    DroolsClientsService droolsClientsService;
+    private Client testSumClient;
 
+    private Client testBigClient;
 
-    @Test
-    public void testRunSpent() throws Exception {
-        Client testSumClient = new Client();
+    @Before
+    public void init() {
+
+        testSumClient = new Client();
         testSumClient.setClientId(1L);
 
         List<Subscriber> subscribers = new ArrayList<Subscriber>();
@@ -40,10 +42,41 @@ public class DroolsClientsServiceImplTest {
         }
         testSumClient.setSubscribers(subscribers);
 
+        testBigClient = new Client();
+        testBigClient.setClientId(2L);
+
+        subscribers = new ArrayList<Subscriber>();
+        for (int i = 1; i < 102; i++) {
+            Subscriber subscriber = new Subscriber();
+            subscriber.setId(i);
+            subscriber.setSpent(100L);
+            subscribers.add(subscriber);
+        }
+        testBigClient.setSubscribers(subscribers);
+    }
+
+    @Autowired
+    @Qualifier("DroolsClientsServiceImpl")
+    DroolsClientsService droolsClientsService;
+
+
+    @Test
+    public void testSpentTotal() throws Exception {
+
         DroolsClient droolsClient = new DroolsClient(testSumClient, new ClientResult());
 
         droolsClientsService.process(newArrayList(droolsClient));
 
         assertEquals(25L, droolsClient.getClientResult().getSpentTotal());
+    }
+
+    @Test
+    public void testIsBig() throws Exception {
+
+        DroolsClient droolsClient = new DroolsClient(testBigClient, new ClientResult());
+
+        droolsClientsService.process(newArrayList(droolsClient));
+
+        assertTrue(droolsClient.getClientResult().isBig());
     }
 }
