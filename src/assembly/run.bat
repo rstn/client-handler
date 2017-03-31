adeb2c7f07173fe2f5e21ec26b93be33ea0e0079
Petr <stone.ptr@gmail.com>
2017-03-31 21:17:06 +0400

    сборка

--- src/assembly/run.bat
+++ src/assembly/run.bat
@@ -1,1 +1,3 @@
+chcp 1251
 java -jar ClientHandlerApp.jar
+@pause
--- src/main/java/com/simbirsoft/drools/clienthandler/app/ClientHandlerApp.java
+++ src/main/java/com/simbirsoft/drools/clienthandler/app/ClientHandlerApp.java
@@ -1,17 +1,24 @@
 package com.simbirsoft.drools.clienthandler.app;
 
 import com.simbirsoft.drools.clienthandler.service.ClientProcessingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ClientHandlerApp {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientHandlerApp.class);
+
     private ClientHandlerApp() {}
 
     public static void main(String[] args) {
+        LOGGER.info("Инициализация");
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         ClientProcessingService clientProcessingMachine = (ClientProcessingService) context.getBean("DroolsClientProcessing");
+        LOGGER.info("Старт обработки файлов");
         clientProcessingMachine.startProcess();
+        LOGGER.info("Обработка файлов завершена");
     }
 }
